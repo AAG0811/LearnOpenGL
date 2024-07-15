04537a7bd4e418849cb22c58cac24d8d27cc93e2
Aidan Grier <aidangrier06@gmail.com>
2024-07-15 19:53:14 +1000

    added camera class

--- camera.h
+++ camera.h
@@ -45,6 +45,72 @@ public:
     Pitch = pitch;
     updateCameraVectors();
   }
-};
+  // constructor with scalars
+  Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch) : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
+  {
+    Position = glm::vec3(posX, posY, posZ);
+    WorldUp = glm::vec3(upX, upY, upZ);
+    Yaw = yaw;
+    Pitch = pitch;
+    updateCameraVectors();
+  }
+  // return view matrix
+  glm::mat4 GetViewMatrix()
+  {
+    return glm::lookAt(Position, Position + Front, Up);
+  }
+  // process keyboard
+  void ProcessKeyboard(Camera_Movement direction, float deltaTime)
+  {
+    float velocity = MovementSpeed * deltaTime;
+    if (direction == FORWARD)
+      Position += Front * velocity;
+    if (direction == BACKWARD)
+      Position -= Front * velocity;
+    if (direction == LEFT)
+      Position -= Right * velocity;
+    if (direction == RIGHT)
+      Position += Right * velocity;
+  }
+  // process mouse
+  void ProcessMouseMovement(float xoffset, float yoffset, GLboolean constrainPitch = true)
+  {
+    xoffset *= MouseSensitivity;
+    yoffset *= MouseSensitivity;
+    Yaw += xoffset;
+    Pitch += yoffset;
+    // clip pitch
+    if (constrainPitch)
+    {
+      if (Pitch > 89.0f)
+        Pitch = 89.0f;
+      if (Pitch < -89.0f)
+        Pitch = -89.0f;
+    }
+    // update vectors
+    updateCameraVectors();
+  }
+  // process mouse scroll
+  void ProcessMouseScroll(float yoffset)
+  {
+    Zoom -= (float)yoffset;
+    if (Zoom < 1.0f)
+      Zoom = 1.0f;
+    if (Zoom > 45.0f)
+      Zoom = 45.0f;
+  }
 
+private:
+  void updateCameraVectors()
+  {
+    glm::vec3 front;
+    front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+    front.y = sin(glm::radians(Pitch));
+    front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
+    Front = glm::normalize(front);
+    // recalute up and right vectors
+    Right = glm::normalize(glm::cross(Front, WorldUp));
+    Up = glm::normalize(glm::cross(Right, Front));
+  }
+};
 #endif
