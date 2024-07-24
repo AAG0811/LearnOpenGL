d5773a0c9398bd588451e67b5c32801002fc7010
Icaangenio <orangehornet921@gmail.com>
2024-07-25 09:58:00 +1000

    added skybox

--- main.cpp
+++ main.cpp
@@ -85,16 +85,27 @@ int main() {
   glEnable(GL_CULL_FACE);
 
   // setup dearimgui
-    // -----------------------------
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO &io = ImGui::GetIO();
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+  // -----------------------------
+  IMGUI_CHECKVERSION();
+  ImGui::CreateContext();
+  ImGuiIO &io = ImGui::GetIO();
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
 
-    // imgui backend
-    ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init();
+  // imgui backend
+  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplOpenGL3_Init();
 
+  // skybox setup
+  vector<std::string> faces
+  {
+      "../assests/skybox/right.jpg",
+      "../assests/skybox/left.jpg",
+      "../assests/skybox/top.jpg",
+      "../assests/skybox/bottom.jpg",
+      "../assests/skybox/front.jpg",
+      "../assests/skybox/back.jpg",
+  };
+  unsigned int cubemapTexture = loadCubeMap(faces);
   // build and compile our shader program
   // ------------------------------------
   Shader modelShader("../shaders/vertShader.vs", "../shaders/fragShader.fs");
@@ -119,106 +130,166 @@ int main() {
       // change model here
       Model ourModel("../assets/models/pistol/pistol.obj");
       Model bModel("../assets/models/sword/sword.obj");
+      // skybox
+    float skyboxVertices[] = {
+    // positions          
+    -1.0f,  1.0f, -1.0f,
+    -1.0f, -1.0f, -1.0f,
+     1.0f, -1.0f, -1.0f,
+     1.0f, -1.0f, -1.0f,
+     1.0f,  1.0f, -1.0f,
+    -1.0f,  1.0f, -1.0f,
 
-      // draw wireframe
-      // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    -1.0f, -1.0f,  1.0f,
+    -1.0f, -1.0f, -1.0f,
+    -1.0f,  1.0f, -1.0f,
+    -1.0f,  1.0f, -1.0f,
+    -1.0f,  1.0f,  1.0f,
+    -1.0f, -1.0f,  1.0f,
 
-      // render loop
-      // -----------
-      while (!glfwWindowShouldClose(window))
-      {
-        // delta time
-        float currentFrame = glfwGetTime();
-        deltaTime = currentFrame - lastFrame;
-        lastFrame = currentFrame;
-        // input
-        // -----
-        processInput(window);
+     1.0f, -1.0f, -1.0f,
+     1.0f, -1.0f,  1.0f,
+     1.0f,  1.0f,  1.0f,
+     1.0f,  1.0f,  1.0f,
+     1.0f,  1.0f, -1.0f,
+     1.0f, -1.0f, -1.0f,
 
-        // render
-        // ------
-        glClearColor(0.1f, 0.10f, 0.10f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glCullFace(GL_BACK);
-        // be sure to activate shader when setting uniforms/drawing objects
-        modelShader.use();
+    -1.0f, -1.0f,  1.0f,
+    -1.0f,  1.0f,  1.0f,
+     1.0f,  1.0f,  1.0f,
+     1.0f,  1.0f,  1.0f,
+     1.0f, -1.0f,  1.0f,
+    -1.0f, -1.0f,  1.0f,
 
-        // view/projection transformations
-        glm::mat4 projection =
-            glm::perspective(glm::radians(camera.Zoom),
-                             (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-        glm::mat4 view = camera.GetViewMatrix();
-        modelShader.setMat4("projection", projection);
-        modelShader.setMat4("view", view);
+    -1.0f,  1.0f, -1.0f,
+     1.0f,  1.0f, -1.0f,
+     1.0f,  1.0f,  1.0f,
+     1.0f,  1.0f,  1.0f,
+    -1.0f,  1.0f,  1.0f,
+    -1.0f,  1.0f, -1.0f,
 
-        // light properties
-        // directional light
-        modelShader.setVec3("dirLight.direction", dirLightDirection);
-        modelShader.setVec3("dirLight.ambient", dirLightAmbient);
-        modelShader.setVec3("dirLight.diffuse", dirLightDiffuse);
-        modelShader.setVec3("dirLight.specular", dirLightSpecular);
-        // point light 1
-        modelShader.setVec3("pointLight.position", pointLightPosition);
-        modelShader.setVec3("pointLight.ambient", pointLightAmbient);
-        modelShader.setVec3("pointLight.diffuse", pointLightDiffuse);
-        modelShader.setVec3("pointLight.specular", pointLightSpecular);
-        modelShader.setFloat("pointLight.constant", pointLightConstant);
-        modelShader.setFloat("pointLight.linear", pointLightLinear);
-        modelShader.setFloat("pointLight.quadratic", pointLightQuadratic);
-        // material properties
-        modelShader.setFloat("material.shininess", materialShininess);
+    -1.0f, -1.0f, -1.0f,
+    -1.0f, -1.0f,  1.0f,
+     1.0f, -1.0f, -1.0f,
+     1.0f, -1.0f, -1.0f,
+    -1.0f, -1.0f,  1.0f,
+     1.0f, -1.0f,  1.0f
+    };
+    unsigned int skyboxVAO, skyboxVBO;
+    glGenVertexArrays(1, &skyboxVAO);
+    glGenBuffers(1, &skyboxVBO);
 
-        // render loaded model
-        glm::mat4 model = glm::mat4(1.0f);
-        model = glm::translate(model, glm::vec3(0.0f, 0.0f, 1.0f));
-        model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-        model = glm::scale(model, glm::vec3(2.0f, 2.0f, 2.0f));
-        modelShader.setMat4("model", model);
-        ourModel.Draw(modelShader);
-        // render loaded model
-        glm::mat4 modelb = glm::mat4(1.0f);
-        modelb = glm::translate(modelb, glm::vec3(0.0f, 0.0f, -5.0f));
-        modelb = glm::scale(modelb, glm::vec3(1.0f, 1.0f, 1.0f));
-        modelShader.setMat4("model", modelb);
-        bModel.Draw(modelShader);
+    glBindVertexArray(skyboxVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+    glBufferData(skyboxVBO, sizeof(skyboxVertices), &skyboxVertices, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+    glBindVertexArray(0);
+    // draw wireframe
+    // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved
-        // etc.)
-        // -------------------------------------------------------------------------------
-        glfwPollEvents();
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+      // delta time
+      float currentFrame = glfwGetTime();
+      deltaTime = currentFrame - lastFrame;
+      lastFrame = currentFrame;
+      // input
+      // -----
+      processInput(window);
 
-        // Start the Dear ImGui frame
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplGlfw_NewFrame();
-        ImGui::NewFrame();
-        // set window size and pos
-        ImGui::SetNextWindowPos(ImVec2(0.0f, 0.0f));
-        ImGui::SetNextWindowSize(ImVec2((float)(SCR_WIDTH * 0.45), (float)(SCR_WIDTH * 0.45)));
-        { // IMGUI Scene Window
-          ImGui::Begin("Scene Settings", NULL, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove);
-          ImGui::Text("Directional Light Settings");
-          ImGui::SliderFloat3("Direction", (float *)&dirLightDirection, -10, 10, "%.3f");
-          ImGui::SliderFloat3("Directional Ambient", (float *)&dirLightAmbient, 0, 10, "%.3f");
-          ImGui::SliderFloat3("Directional Diffuse", (float *)&dirLightDiffuse, 0, 10, "%.3f");
-          ImGui::SliderFloat3("Directional Specular", (float *)&dirLightSpecular, 0, 10, "%.3f");
-          ImGui::Text("Point Light Settings");
-          ImGui::SliderFloat3("Point Position", (float *)&pointLightPosition, -10, 10, "%.3f");
-          ImGui::SliderFloat3("Point Ambient", (float *)&pointLightAmbient, 0, 10, "%.3f");
-          ImGui::SliderFloat3("Point Diffuse", (float *)&pointLightDiffuse, 0, 10, "%.3f");
-          ImGui::SliderFloat3("Point Specular", (float *)&pointLightSpecular, 0, 10, "%.3f");
-          ImGui::SliderFloat("Point Constant", (float *)&pointLightConstant, 0, 10, "%.3f");
-          ImGui::SliderFloat("LPoint inear", (float *)&pointLightLinear, 0, 5, "%.3f");
-          ImGui::SliderFloat("Point Quadratic", (float *)&pointLightQuadratic, 0, 5, "%.4f");
-          ImGui::Text("Material Settings");
-          ImGui::SliderFloat("Material Shininess", (float *)&materialShininess, 0, 100, "%.2f");
-          if(ImGui::Button("Close Program"))
-            glfwSetWindowShouldClose(window, true);
-          ImGui::End();
-        }
-        ImGui::Render();
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+      // render
+      // ------
+      glClearColor(0.1f, 0.10f, 0.10f, 1.0f);
+      glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+      glCullFace(GL_BACK);
+      // be sure to activate shader when setting uniforms/drawing objects
+      modelShader.use();
 
-        glfwSwapBuffers(window);
+      // view/projection transformations
+      glm::mat4 projection =
+          glm::perspective(glm::radians(camera.Zoom),
+                           (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
+      glm::mat4 view = camera.GetViewMatrix();
+      modelShader.setMat4("projection", projection);
+      modelShader.setMat4("view", view);
+
+      // set skybox
+      glBindVertexArray(skyboxVAO);
+      glBindTexture(GL_TEXTURE_CUBE_MAP, cubemapTexture);
+      glDrawArrays(GL_TRIANGLES, 0, 36);
+      glDepthMask(GL_TRUE);
+
+      // light properties
+      // directional light
+      modelShader.setVec3("dirLight.direction", dirLightDirection);
+      modelShader.setVec3("dirLight.ambient", dirLightAmbient);
+      modelShader.setVec3("dirLight.diffuse", dirLightDiffuse);
+      modelShader.setVec3("dirLight.specular", dirLightSpecular);
+      // point light 1
+      modelShader.setVec3("pointLight.position", pointLightPosition);
+      modelShader.setVec3("pointLight.ambient", pointLightAmbient);
+      modelShader.setVec3("pointLight.diffuse", pointLightDiffuse);
+      modelShader.setVec3("pointLight.specular", pointLightSpecular);
+      modelShader.setFloat("pointLight.constant", pointLightConstant);
+      modelShader.setFloat("pointLight.linear", pointLightLinear);
+      modelShader.setFloat("pointLight.quadratic", pointLightQuadratic);
+      // material properties
+      modelShader.setFloat("material.shininess", materialShininess);
+
+      // render loaded model
+      glm::mat4 model = glm::mat4(1.0f);
+      model = glm::translate(model, glm::vec3(0.0f, 0.0f, 1.0f));
+      model = glm::rotate(model, glm::radians(90.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+      model = glm::scale(model, glm::vec3(2.0f, 2.0f, 2.0f));
+      modelShader.setMat4("model", model);
+      ourModel.Draw(modelShader);
+      // render loaded model
+      glm::mat4 modelb = glm::mat4(1.0f);
+      modelb = glm::translate(modelb, glm::vec3(0.0f, 0.0f, -5.0f));
+      modelb = glm::scale(modelb, glm::vec3(1.0f, 1.0f, 1.0f));
+      modelShader.setMat4("model", modelb);
+      bModel.Draw(modelShader);
+
+      // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved
+      // etc.)
+      // -------------------------------------------------------------------------------
+      glfwPollEvents();
+
+      // Start the Dear ImGui frame
+      ImGui_ImplOpenGL3_NewFrame();
+      ImGui_ImplGlfw_NewFrame();
+      ImGui::NewFrame();
+      // set window size and pos
+      ImGui::SetNextWindowPos(ImVec2(0.0f, 0.0f));
+      ImGui::SetNextWindowSize(ImVec2((float)(SCR_WIDTH * 0.45), (float)(SCR_WIDTH * 0.45)));
+      { // IMGUI Scene Window
+        ImGui::Begin("Scene Settings", NULL, ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove);
+        ImGui::Text("Directional Light Settings");
+        ImGui::SliderFloat3("Direction", (float *)&dirLightDirection, -10, 10, "%.3f");
+        ImGui::SliderFloat3("Directional Ambient", (float *)&dirLightAmbient, 0, 10, "%.3f");
+        ImGui::SliderFloat3("Directional Diffuse", (float *)&dirLightDiffuse, 0, 10, "%.3f");
+        ImGui::SliderFloat3("Directional Specular", (float *)&dirLightSpecular, 0, 10, "%.3f");
+        ImGui::Text("Point Light Settings");
+        ImGui::SliderFloat3("Point Position", (float *)&pointLightPosition, -10, 10, "%.3f");
+        ImGui::SliderFloat3("Point Ambient", (float *)&pointLightAmbient, 0, 10, "%.3f");
+        ImGui::SliderFloat3("Point Diffuse", (float *)&pointLightDiffuse, 0, 10, "%.3f");
+        ImGui::SliderFloat3("Point Specular", (float *)&pointLightSpecular, 0, 10, "%.3f");
+        ImGui::SliderFloat("Point Constant", (float *)&pointLightConstant, 0, 10, "%.3f");
+        ImGui::SliderFloat("LPoint inear", (float *)&pointLightLinear, 0, 5, "%.3f");
+        ImGui::SliderFloat("Point Quadratic", (float *)&pointLightQuadratic, 0, 5, "%.4f");
+        ImGui::Text("Material Settings");
+        ImGui::SliderFloat("Material Shininess", (float *)&materialShininess, 0, 100, "%.2f");
+        if (ImGui::Button("Close Program"))
+          glfwSetWindowShouldClose(window, true);
+        ImGui::End();
+      }
+      ImGui::Render();
+      ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+      glfwSwapBuffers(window);
   }
   // shutdown imgui
     ImGui_ImplOpenGL3_Shutdown();
